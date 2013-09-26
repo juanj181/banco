2f0dc33b8f32ec6dfd3906639220a04d071020f7
alumno <alumno@2A5PC04>
2013-09-26 10:43:54 +0200

    primer commit con el programa de bancos, tambien incluye base de datos

---
+++ src/com/fpmislata/banco/Banco.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fpmislata.banco;
+
+/**
+ *
+ * @author alumno
+ */
+public class Banco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    
+
+   EntidadBancaria bankia = new EntidadBancaria();
+        
+    }
+            
+            
+}
+
