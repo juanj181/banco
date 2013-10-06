bf82db6a9d8e88c0493d3bcb62c18d187a6a30c4
Javi <Javi@Javi-PC>
2013-10-06 19:58:56 +0200

    Conexion a la base de datos, metodo read hecho

--- src/com/fpmislata/banco/Banco.java
+++ src/com/fpmislata/banco/Banco.java
@@ -4,6 +4,12 @@
  */
 package com.fpmislata.banco;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author alumno
@@ -13,15 +19,32 @@ public class Banco {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-    
+    // TODO code application logic here
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        //System.out.println("Se ha conectado a la BD!!");
+        read( " SELECT * from entidadbancaria WHERE idEntidadBancaria=? ");
 
-   EntidadBancaria bankia = new EntidadBancaria();
-        
     }
-            
-            
-}
+    public static void read(String mSQL)throws SQLException, ClassNotFoundException{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = null;
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/banco", "banco", "banco");
+        String selectSQL = mSQL;
+        PreparedStatement preparedStatement = connection.prepareStatement(selectSQL);
+        preparedStatement.setInt(1, 1);
+        ResultSet rs = preparedStatement.executeQuery();
 
+        while (rs.next()) {
+            int idEntidad = rs.getInt("idEntidadBancaria");
+            int CodigoEntidad = rs.getInt("codigoEntidad");
+            String Nombre = rs.getString("nombre");
+            String cif = rs.getString("cif");
+            System.out.println("ID " + "CódigoEntidad  " + "NombreEntidad " + "Cif ");
+            System.out.println(" " + idEntidad + "     " + CodigoEntidad + "     " + Nombre + "     " + cif);
+        }
+
+
+        connection.close();
+       
+    }
+}
